1dddf41687205f2804e5097c05321b4b7b4027ed
RobbyT <robertet4@gmail.com>
2026-05-21 12:38:28 -0400

    Cleaned up main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from time import sleep
 
 from runelite_library.login import LoginLogout
-from runelite_library.player import Player
-from runelite_library.rune_logger import log_event
 from runelite_library.window_management import RuneLiteWindow
-from tasks.birdhouse_run import BirdhouseRun, State
-from too_many_items import MiscObjects, PathingObjects
+from tasks.birdhouse_run import BirdhouseRun
 
 rl = RuneLiteWindow()
 rl.activate()
