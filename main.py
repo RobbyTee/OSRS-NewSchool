77f49b3be4b8307a55e0fc22a6f10d872c91dc08
RobbyT <robertet4@gmail.com>
2026-05-21 20:25:46 -0400

    Added bedtime

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime, time
 from time import sleep
 
 from runelite_library.login import LoginLogout
@@ -8,34 +9,48 @@ from tasks.birdhouse_run import BirdhouseRun
 
 
 def random_interval():
-    return random.unifom(50, 75)
+    return random.uniform(50, 75)
 
 
-rl = RuneLiteWindow()
-rl.activate()
-sleep(0.5)
+def bedtime():
+    now = datetime.now().time()
+    start = time(20, 0)
+    end = time(5, 0)
 
-login = LoginLogout(rl)
+    return now >= start or now <= end
 
-bh = BirdhouseRun(rl)
-last_bh = TrackTask("birdhouse_run")
 
-do_task = False
+def main():
+    rl = RuneLiteWindow()
+    rl.activate()
+    sleep(0.5)
 
-# print(last_bh.time_since_task())
+    login = LoginLogout(rl)
 
-while True:
-    if last_bh.time_since_task() > 50:
-        do_task = True
+    bh = BirdhouseRun(rl)
+    last_bh = TrackTask("birdhouse_run")
 
-    if do_task:
-        login.login_now()
+    do_task = False
 
-        if bh.main():
-            sleep(30)
-            login.logout_now()
-            do_task = False
-        else:
-            break
+    while True:
+        if bedtime():
+            sleep(3600)
 
-    sleep(random_interval() * 60)
+        if last_bh.time_since_task() > 50:
+            do_task = True
+
+        if do_task:
+            login.login_now()
+
+            if bh.main():
+                sleep(30)
+                login.logout_now()
+                do_task = False
+            else:
+                break
+
+        sleep(random_interval() * 60)
+
+
+if __name__ == "__main__":
+    main()
